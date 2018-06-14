419028ce2a6e24c54b6d1c313c8408a285d2f515
alisonamerico <alison.americo@gmail.com>
2018-06-14 00:08:26 -0300

    Instalado pytest-django e criado os primeiros testes na home.
    close #25

--- rebirth/core/tests.py
+++ rebirth/core/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django_assertions import dj_assert_contains
 
-# Create your tests here.
+
+def test_status_code(client):
+    response = client.get('/')
+    assert 200 == response.status_code
+
+
+def test_home(client):
+    response = client.get('/')
+    dj_assert_contains(response, 'Olá Mundo')
