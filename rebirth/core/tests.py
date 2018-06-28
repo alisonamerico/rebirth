1cdb5bb05b40f7ccede90e882740d2d0ba28d256
alisonamerico <alison.americo@gmail.com>
2018-06-27 21:52:57 -0300

    Criada aba Contato
    close #57

--- rebirth/core/tests.py
+++ rebirth/core/tests.py
@@ -1,3 +1,5 @@
+import pytest
+
 from django_assertions import dj_assert_contains
 
 
@@ -6,6 +8,33 @@ def test_status_code(client):
     assert 200 == response.status_code
 
 
-def test_home(client):
+@pytest.mark.parametrize(
+    'content', [
+        'COK Store',
+        'Contato',
+        'Copyright',
+        'href="/contato"',
+    ]
+)
+def test_home(client, content):
     response = client.get('/')
-    dj_assert_contains(response, 'COK Store')
+    dj_assert_contains(response, content)
+
+
+def test_contact_status_code(client):
+    response = client.get('/contato/')
+    assert 200 == response.status_code
+
+
+@pytest.mark.parametrize(
+    'content', [
+        'Rua Jataúba 161',
+        'Recife, PE',
+        '(81) 9 8888-8888',
+        'name@example.com',
+        'Segunda - Sexta: 9:00 às 17:00',
+    ]
+)
+def test_contact_content(client, content):
+    response = client.get('/contato/')
+    dj_assert_contains(response, content)
